8ee3d1d4e173f6270da11fbf2683da8033efd908
Sally <78157471+codesals@users.noreply.github.com>
2021-02-01 19:25:47 +0300

    added story.js & updated variables

---
+++ story.js
@@ -0,0 +1,9 @@
+let fname = "Haya";
+let age = 27;
+let hobby = "cycling";
+
+console.log(
+  `Mr./Mrs. ${fname}, in ${age} years, you will be ${
+    age + 33
+  } years old. By then you'll have decades of ${hobby} practice!`
+);
